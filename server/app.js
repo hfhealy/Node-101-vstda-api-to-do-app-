2564c7d6c8a4be676381f20fdc556127f367ca00
Hannah <hfhealy@gmail.com>
2022-04-19 11:04:50 -0700

    updated

--- server/app.js
+++ server/app.js
@@ -24,6 +24,7 @@ var list = [
 	}
 ];
 
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -43,16 +44,38 @@ app.get('/api/TodoItems', (req, res) => {
 });
 
 app.get('/api/TodoItems/:number', (req, res) => {
-    // console.log("line 46", list[req.params.number]);
-    res.json(list[req.params.number]);
+	for (let i=0; i<list.length; i++) {
+		if (list[i].todoItemId == req.params.number) {
+			res.status(200).json(list[i]);
+			return;
+		} 
+	}
 });
 
 app.post('/api/TodoItems', (req, res) => {
-    res.status(201).json(req.body);
+    //Add an item to the dataset. 
+	//If there is already an item with a matching todoItemId, 
+	//overwrite the existing item.
+
+	for (let i=0; i<list.length; i++) {
+		if (list[i].todoItemId == req.body.todoItemId) {
+			list.splice(i, 1, req.body);
+			res.status(201).json(req.body);
+			return;
+		} 
+	}
+	list.push(req.body);
+	
 });
 
 app.delete('/api/TodoItems/:number', (req, res) => {
-    res.json(list[req.params.number]);
+	for(let i=0; i<list.length; i++) {
+		if (list[i].todoItemId == req.params.number) {
+			res.json(list[i]);
+			list.splice(i, 1);
+			return;
+		}
+	}
 });
 
 module.exports = app;
